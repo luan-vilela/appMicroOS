3d4472ef33b1cb023fcceed0e90454b28d0e77e3
luan <luan@debian>
2020-01-09 18:00:26 -0400

    Criação index

--- app.js
+++ app.js
@@ -18,7 +18,17 @@ app.set('view engine', 'handlebars');
  app.get('/', function(req, res){
     res.render('home');
  });
+ app.get('/login', function(req, res){
+    res.render('login');
+ });
+app.post('/acesso', function(req, res){
+    res.send("logado")
+});
 
+// Página não encontrada ou erros
+app.use(function(req, res, next) {
+    res.send('OK, algo de certo está errado!');
+  });
 
 app.listen(3000, function(){
     console.log('Servidor iniciado.');
